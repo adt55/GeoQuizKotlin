1148981c8b945a3bb71df27221fb75b2070d9fa4
adt55 <adt55@pitt.edu>
2023-02-16 19:59:26 -0500

    Completion of kotlin
    quiz done

--- Project/app/src/main/java/com/athompson/geoquiz/Question.kt
+++ Project/app/src/main/java/com/athompson/geoquiz/Question.kt
@@ -1,3 +1,3 @@
 package com.athompson.geoquiz
 import androidx.annotation.StringRes
-data class Question(@StringRes val textRedId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
